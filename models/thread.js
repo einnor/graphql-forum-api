f7199b719d20f3eb0878bdabc25c5e5aedd6eadd
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-01 17:43:22 +0300

    Thread model

---
+++ models/thread.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Thread = sequelize.define('Thread', {
+    title: DataTypes.STRING
+  }, {});
+  Thread.associate = function(models) {
+    // associations can be defined here
+  };
+  return Thread;
+};
