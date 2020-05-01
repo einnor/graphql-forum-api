1152788607f66713822d1aaf5f8678c4a8bc66ec
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-01 19:42:18 +0300

    Thread typedefs

---
+++ TypeDefs/thread.js
@@ -0,0 +1,23 @@
+const { gql } = require('apollo-server-express');
+
+module.exports = gql`
+  type Thread {
+    id: ID!
+    title: String!
+    slug: String!
+    slug: String!
+    creator: User!
+    channel: Channel!
+    status: ThreadStatus!
+    isLocked: Boolean!
+    lastRepliedAt: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+`;
+
+enum ThreadStatus {
+  SOLVED
+  UNSOLVED
+}
+
