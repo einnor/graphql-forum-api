d3c3547561a5c54f5d525339a58da9de89a262cf
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-01 19:34:30 +0300

    User typedefs

---
+++ TypeDefs/user.js
@@ -0,0 +1,17 @@
+const { gql } = require('apollo-server-express');
+
+module.exports = gql`
+  type User {
+    id: ID!
+    username: String!
+    role: Role!
+    avatar: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+`;
+
+enum Role {
+  ADMIN
+  USER
+}
