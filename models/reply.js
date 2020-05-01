3b8b7267a04b03246f6ce91c7c5d90b69c64380a
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-01 17:52:44 +0300

    Updated reply model

--- models/reply.js
+++ models/reply.js
@@ -1,7 +1,38 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Reply = sequelize.define('Reply', {
-    title: DataTypes.STRING
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    threadID: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    isBestAnswer: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
   }, {});
   Reply.associate = function(models) {
     // associations can be defined here
