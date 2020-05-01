1584b14b235c8a65da1564495a323772c2666181
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-01 18:20:39 +0300

    Debugging

--- migrations/20200501143736-create-channel.js
+++ migrations/20200501143736-create-channel.js
@@ -4,7 +4,6 @@ module.exports = {
     return queryInterface.createTable('Channels', {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
         type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4
--- migrations/20200501144353-create-reply.js
+++ migrations/20200501144353-create-reply.js
@@ -4,7 +4,6 @@ module.exports = {
     return queryInterface.createTable('Replies', {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
         type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4
