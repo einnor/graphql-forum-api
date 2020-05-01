8a4d885e6a360e13e7106e38efadbf7b50e03293
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-01 17:49:47 +0300

    Updated thread model

--- models/thread.js
+++ models/thread.js
@@ -1,7 +1,55 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Thread = sequelize.define('Thread', {
-    title: DataTypes.STRING
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    slug: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    channelId: {
+      type: DataTypes.UUID,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.ENUM('UNSOLVED', 'SOLVED'),
+      allowNull: false,
+      defaultValue: 'UNSOLVED'
+    },
+    isLocked: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    lastRepliedAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
   }, {});
   Thread.associate = function(models) {
     // associations can be defined here
