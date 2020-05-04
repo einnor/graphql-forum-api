6dad599b3332619b3285f418b45ae7e9bf36c6c1
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-04 11:30:17 +0300

    Throw apollo error is user exists

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,9 +1,15 @@
+const { ApolloError} = require('apollo-server-express');
+
 module.exports = {
   Mutation: {
     async signUp (parent, args, context) {
       const { models } = context;
       const { username, email, password } = args;
-      const userExusts = await models.User.findOne({ where: { email } });
+      const userExists = await models.User.findOne({ where: { email } });
+
+      if (userExists) {
+        throw new ApolloError('User already exists.');
+      }
     },
   },
 };
