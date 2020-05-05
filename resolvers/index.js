88bd87988f561fd78073c4d92afec8cd8b2d7f24
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-05 19:41:19 +0300

    Export favorite resolvers

--- resolvers/index.js
+++ resolvers/index.js
@@ -3,6 +3,7 @@ const user = require('./user');
 const channel = require('./channel');
 const thread = require('./thread');
 const reply = require('./reply');
+const favorite = require('./favorite');
 
 module.exports = [
   datetime,
@@ -10,4 +11,5 @@ module.exports = [
   channel,
   thread,
   reply,
+  favorite,
 ];
