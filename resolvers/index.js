855e370cde5a49a1671773fa49e2a056b42bfae0
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-05 18:46:46 +0300

    Export the resolver

--- resolvers/index.js
+++ resolvers/index.js
@@ -2,10 +2,12 @@ const datetime = require('./datetime');
 const user = require('./user');
 const channel = require('./channel');
 const thread = require('./thread');
+const reply = require('./reply');
 
 module.exports = [
   datetime,
   user,
   channel,
   thread,
+  reply,
 ];
