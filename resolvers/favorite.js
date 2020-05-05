6e4189c15fc32c8585ed997395f6741d100d7562
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-05 19:40:41 +0300

    Resolve the reply field

--- resolvers/favorite.js
+++ resolvers/favorite.js
@@ -5,6 +5,11 @@ module.exports = {
     user (favorite, args, context) {
       const { models } = context;
       return models.User.findByPk(favorite.userId);
+    },
+
+    reply (favorite, args, context) {
+      const { models } = context;
+      return models.Reply.findByPk(favorite.replyId);
     }
   },
 };
