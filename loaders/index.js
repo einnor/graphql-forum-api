9a9635d243a2ca1238ad9e1cf613456d0e410f61
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-08 22:30:08 +0300

    Export loader

--- loaders/index.js
+++ loaders/index.js
@@ -1,9 +1,11 @@
 const userLoader = require('./user');
 const channelLoader = require('./channel');
 const repliesLoader = require('./replies');
+const favoritesLoader = require('./favorites');
 
 module.exports = {
   userLoader,
   channelLoader,
   repliesLoader,
+  favoritesLoader,
 };
