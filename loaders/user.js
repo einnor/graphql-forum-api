89dd20e16a51662f88d5f7144aebd81f5f2b4200
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-08 21:35:18 +0300

    Function to batchUsers

--- loaders/user.js
+++ loaders/user.js
@@ -2,4 +2,16 @@ const DataLoader = require('dataloader');
 
 const models = require('../models');
 
+const batchUsers = async (ids, models) => {
+  const users = await models.User.findAll({
+    where: {
+      id: {
+        [models.Sequelize.Op.in]: ids,
+      },
+    },
+  });
+
+  return ids.map(id => users.find(user => user.id === id));
+};
+
 const loader = new DataLoader(ids => batchUsers(ids, models));
